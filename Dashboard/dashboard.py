8d5442c648140ad8ed6c67518a10b7b1abcde928
Ansikte <ali.hussian@iths.se>
2023-11-22 08:42:10 +0100

    Add server to dashboard.py

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -2,6 +2,7 @@ import dash
 from layout import create_layout
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True)
+server = app.server
 app.layout = create_layout()
 
 if __name__ == '__main__':
