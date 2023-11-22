98bc693e7101cd01aa01ee75b9aa023105850c63
Ansikte <ali.hussian@iths.se>
2023-11-22 11:11:46 +0100

    Add searchable=False in dcc.Dropdown

--- Dashboard/layout.py
+++ Dashboard/layout.py
@@ -27,7 +27,8 @@ def create_layout():
         dcc.Dropdown(
             id="dropdown-menu",
             options=dropdown_options,
-            value="sex_distribution"  # Default selected value
+            value="sex_distribution",  # Default selected value
+            searchable=False # To prevent keyboard popping up on phones
         ),
 
         # Graph container
